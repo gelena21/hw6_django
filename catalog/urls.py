22ca93693184887cc66d9143d9a7ba75a16cb329
gelena <helenatcitok@gmail.com>
2024-03-04 13:17:28 +0000

    hw done I hope

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from catalog.views import index, contacts, product_list
+from catalog.views import contacts, product_list, product_detail
 
 urlpatterns = [
     path('', product_list),
     path('contacts/', contacts),
-    path('products/<int:pk>/', product_list),
+    path('products/<int:pk>/', product_detail),
 ]
